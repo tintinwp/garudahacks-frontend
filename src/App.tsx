56de56153c887e8269b307e3a3fe0e5ae248c277
Michael Bryan  Chandra <89082382+liquedgit@users.noreply.github.com>
2024-07-12 23:53:01 +0700

    ft: modal upon new user

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as RouterProvider } from "react-router-dom"
-import Routing from "./routes/routes"
-import AllLayout from "./layouts/all-layout"
+import { BrowserRouter as RouterProvider } from "react-router-dom";
+import Routing from "./routes/routes";
+import AllLayout from "./layouts/all-layout";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { LoadingProvider } from "./context/loading-context";
 import { ApiProvider } from "./context/api-context";
@@ -13,20 +13,20 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-          <LoadingProvider>
+        <LoadingProvider>
+          <RouterProvider>
             <ApiProvider>
-              <RouterProvider>
-                <Provider store={store}>
-                  <AllLayout>
-                    <Routing/>
-                  </AllLayout>
-                </Provider>
-              </RouterProvider>
+              <Provider store={store}>
+                <AllLayout>
+                  <Routing />
+                </AllLayout>
+              </Provider>
             </ApiProvider>
-          </LoadingProvider>
+          </RouterProvider>
+        </LoadingProvider>
       </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/context/api-context.tsx
+++ src/context/api-context.tsx
@@ -9,150 +9,231 @@ import { User } from "@/types/backend/user";
 import { LoginPayload } from "@/types/backend/payload/login-payload";
 import { RegisterPayload } from "@/types/backend/payload/register-payload";
 import { checkUser } from "@/lib/utils";
+import {
+  AlertDialog,
+  AlertDialogFooter,
+  AlertDialogHeader,
+} from "@/components/ui/alert-dialog";
+import {
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { AlertDialogAction } from "@radix-ui/react-alert-dialog";
+import { useNavigate } from "react-router-dom";
 
-export type GetApi<T> = (endpoint: Endpoint, id?: string, queryParameter?: Record<string, string | number | boolean>) => Promise<T | null>;
-export type MutateApi<T> =(endpoint: Endpoint, data?: unknown, id?: string, successMessage?: string) => Promise<T | null>;
+export type GetApi<T> = (
+  endpoint: Endpoint,
+  id?: string,
+  queryParameter?: Record<string, string | number | boolean>
+) => Promise<T | null>;
+export type MutateApi<T> = (
+  endpoint: Endpoint,
+  data?: unknown,
+  id?: string,
+  successMessage?: string
+) => Promise<T | null>;
 
 interface ApiContext {
-  get: GetApi<any>,
-  user: User | null | undefined,
-  mutate: MutateApi<any>,
-  login: (payload: LoginPayload | RegisterPayload) => Promise<void>
+  get: GetApi<any>;
+  user: User | null | undefined;
+  mutate: MutateApi<any>;
+  login: (payload: LoginPayload | RegisterPayload) => Promise<void>;
   logout: () => void;
   refetchUser: () => void;
 }
 
-const LOCAL_STORAGE_ACCESS_TOKEN_KEY = 'HANDY_KEY'
+const LOCAL_STORAGE_ACCESS_TOKEN_KEY = "HANDY_KEY";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_API_URL;
 const apiContext = createContext<ApiContext>({} as ApiContext);
 
+export function ApiProvider({ children }: ChildrenOnly) {
+  const [accessToken, setAccessToken] = useState<string | undefined>(
+    localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+      ? (localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY) as string)
+      : undefined
+  );
+  const [showInitialModal, setShowInitialModal] = useState<boolean>(false);
+  const nav = useNavigate();
+  const { data: user, refetch } = useQuery<User | null, Error>(
+    "authMe",
+    async () => {
+      const resp = await get<User>(endpoints.auth.me);
+      if (resp) {
+        return checkUser(resp) as User;
+      }
+      return null;
+    },
+    {
+      enabled: !!accessToken,
+    }
+  );
 
-
-export function ApiProvider({children}: ChildrenOnly){
-  const [accessToken, setAccessToken] = useState<string | undefined>(localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY) ? localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY) as string : undefined);
-  const { data: user , refetch } = useQuery<User | null, Error>('authMe', async () => {
-   const resp =  await get<User>(endpoints.auth.me)
-   if(resp){
-    return checkUser(resp) as User
-   }
-   return null
-  }, {
-    enabled: !!accessToken,
-  }
-);
- 
-  function buildAxios(token?: string){
-    const instance =  axios.create({
+  function buildAxios(token?: string) {
+    const instance = axios.create({
       baseURL: BACKEND_URL,
       headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-    // Can do interceptor in here 
+    // Can do interceptor in here
     return instance;
   }
-  
+
   const api: AxiosInstance = useMemo(() => {
-    return buildAxios(accessToken)
-  }, [accessToken])
+    return buildAxios(accessToken);
+  }, [accessToken]);
 
   const { setIsLoading } = useLoading();
 
-  function buildUrl(baseUrl: string, id?: string, queryParameter?: Record<string, string | number | boolean>){
+  function buildUrl(
+    baseUrl: string,
+    id?: string,
+    queryParameter?: Record<string, string | number | boolean>
+  ) {
     let url = baseUrl;
     if (id) {
       url += `/${id}`;
     }
-  
+
     if (queryParameter) {
-      const queryParams = new URLSearchParams(queryParameter as Record<string, string>).toString();
+      const queryParams = new URLSearchParams(
+        queryParameter as Record<string, string>
+      ).toString();
       url += `?${queryParams}`;
     }
-  
+
     return url;
   }
 
-  async function get<T>(endpoint: Endpoint, id?: string, queryParameter?: Record<string, string | number | boolean>): Promise<T | null>{
-    try{
+  async function get<T>(
+    endpoint: Endpoint,
+    id?: string,
+    queryParameter?: Record<string, string | number | boolean>
+  ): Promise<T | null> {
+    try {
       const url = buildUrl(endpoint.url, id, queryParameter);
-      const response = await api.get<T>(url)
-      return response.data
-    } catch(err){
-      console.log('[API Error]', err)
-      return null
+      const response = await api.get<T>(url);
+      return response.data;
+    } catch (err) {
+      console.log("[API Error]", err);
+      return null;
     }
   }
 
   // function getErrorMessage(err: AxiosError){
-    // Costumize this to get error emssages
-    // return 'There are error when fetching to endpoints!'
+  // Costumize this to get error emssages
+  // return 'There are error when fetching to endpoints!'
   // }
 
-  async function  mutate<T>(endpoint: Endpoint, data?: unknown, id?: string, successMessage?: string): Promise<T | null> {
+  async function mutate<T>(
+    endpoint: Endpoint,
+    data?: unknown,
+    id?: string,
+    successMessage?: string
+  ): Promise<T | null> {
     setIsLoading(true);
     let response = null;
     const url = buildUrl(endpoint.url, id);
-    try{
-      switch (endpoint.method){
-        case 'DELETE':
-          response = await api.delete<T>(url)
-          break
-        case 'POST':
-          response = await api.post<T>(url, data)
-          break
-        case 'PUT':
-          response = await api.put<T>(url, data)
+    try {
+      switch (endpoint.method) {
+        case "DELETE":
+          response = await api.delete<T>(url);
           break;
-        }
-        if(successMessage){
-          // toastSuccess(successMessage)
-        }
-      } catch(err){
-        if(!import.meta.env.PROD) {
-          console.log('[API Error]', err)
-        }
-        // toastError(getErrorMessage(err));
+        case "POST":
+          response = await api.post<T>(url, data);
+          break;
+        case "PUT":
+          response = await api.put<T>(url, data);
+          break;
       }
-      setIsLoading(false);
-      if(response){
-        return response.data
-      } 
-      return null
+      if (successMessage) {
+        // toastSuccess(successMessage)
+      }
+    } catch (err) {
+      if (!import.meta.env.PROD) {
+        console.log("[API Error]", err);
+      }
+      // toastError(getErrorMessage(err));
+    }
+    setIsLoading(false);
+    if (response) {
+      return response.data;
+    }
+    return null;
   }
 
-  async function login(payload: LoginPayload | RegisterPayload){
-    const response = await mutate<GenerateUserResponse>(endpoints.auth.login, payload)
-    if(response){
-      localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, response.access_token)
-      setAccessToken(response.access_token)
+  async function login(payload: LoginPayload | RegisterPayload) {
+    const response = await mutate<GenerateUserResponse>(
+      endpoints.auth.login,
+      payload
+    );
+    if (response) {
+      localStorage.setItem(
+        LOCAL_STORAGE_ACCESS_TOKEN_KEY,
+        response.access_token
+      );
+      setAccessToken(response.access_token);
     }
   }
 
-  function logout(){
-    localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+  function logout() {
+    localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
   }
 
-  async function fetchGuest(){
-    const localStorageData = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
-    if(!localStorageData) {
-      const resp = await mutate<GenerateUserResponse>(endpoints.auth.generateRequest)
-      if(resp){
-        localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, resp.access_token)
-        setAccessToken(resp.access_token)
+  async function fetchGuest() {
+    const localStorageData = localStorage.getItem(
+      LOCAL_STORAGE_ACCESS_TOKEN_KEY
+    );
+    if (!localStorageData) {
+      setShowInitialModal(true);
+      const resp = await mutate<GenerateUserResponse>(
+        endpoints.auth.generateRequest
+      );
+      if (resp) {
+        localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, resp.access_token);
+        setAccessToken(resp.access_token);
       }
     }
   }
 
   useEffect(() => {
     fetchGuest();
-  }, [])
-  
-  return <apiContext.Provider value={{refetchUser: refetch,logout, get, mutate, user, login }}>
-          {children}
+  }, []);
+
+  useEffect(() => {
+    console.log(showInitialModal);
+  }, [showInitialModal]);
+
+  return (
+    <apiContext.Provider
+      value={{ refetchUser: refetch, logout, get, mutate, user, login }}
+    >
+      <AlertDialog onOpenChange={setShowInitialModal} open={showInitialModal}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>New to handy ?</AlertDialogTitle>
+            <AlertDialogDescription>
+              If you are new to handy, feel free to continue as guest and try
+              out our feature.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction onClick={() => nav("/login")}>
+              Sign in
+            </AlertDialogAction>
+            <AlertDialogAction>Continue as Guest</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      {children}
     </apiContext.Provider>
+  );
 }
 
-export default function useApi(){
+export default function useApi() {
   return useContext(apiContext);
 }
